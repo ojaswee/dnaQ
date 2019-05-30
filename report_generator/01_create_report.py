d6508e2db0776d178776e8de1ae5e304ff73ed42
ojaswee <ojaswee@outlook.com>
2019-05-30 14:23:35 -0500

    added case if the condition or gene is not found

--- report_generator/01_create_report.py
+++ report_generator/01_create_report.py
@@ -3,7 +3,10 @@ import pandas as pd
 import requests as req
 from PyPDF2 import PdfFileMerger
 
-# /opt/python3/bin/python3.4 /home/ojaswee/github/dnaQ/report_generator/01_create_report.py "Global Similarity" "home/ojaswee/dnaq/analysis/2_1_RUN2/Report/condition_gene.csv" "/home/ojaswee/dnaq/analysis/2_1_RUN2/Report/"
+# CREATE A pdf file if file not found
+from fpdf import FPDF
+
+# /opt/python3/bin/python3.4 /home/ojaswee/github/dnaQ/report_generator/01_create_report.py "Global Similarity" "home/ojaswee/dnaq/analysis/2_1_RUN1/Report/condition_gene.csv" "/home/ojaswee/dnaq/analysis/2_1_RUN1/Report/"
 
 SAMPLENAME = sys.argv[1]
 INPUTFILE  = sys.argv[2]
@@ -22,17 +25,33 @@ df.columns=['type','value']
 pdfs = []
 
 for indx,row in df.iterrows():
-    r=req.get(WEBSOURCE+row['type']+"/"+row['value']+".pdf", allow_redirects=True)
-    pdf_file=OUT_DIR+SAMPLENAME+"_"+row['type']+"_"+row['value']+".pdf"
-    open(pdf_file, 'wb').write(r.content)
-    pdfs.append(pdf_file)
+    try:
+        r=req.get(WEBSOURCE+row['type']+"/"+row['value']+".pdf", allow_redirects=True)
+        pdf_file=OUT_DIR+SAMPLENAME+"_"+row['type']+"_"+row['value']+".pdf"
+        open(pdf_file, 'wb').write(r.content)
 
+    except IOError:
+        print("")
+
+    else:
+        r="Cannot find " + row ['type'] + ': ' + row ['value']
+        pdf_file = OUT_DIR+SAMPLENAME+"_"+row['type']+"_"+row['value']+".pdf"
+
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_xy(10, 10)
+        pdf.set_font('arial', 'B', 13.0)
+        pdf.cell(ln=0, h=5.0, align='L', w=0, txt=r, border=0)
+        pdf.output(pdf_file)
+        pdf.close()
+
+    pdfs.append(pdf_file)
 
 merger = PdfFileMerger()
 
 for pdf in pdfs:
-    # print(pdf)
+    # print (pdf)
     merger.append(open(pdf, 'rb'))
 
-    with open(OUT_DIR+SAMPLENAME+"_Result.pdf", 'wb') as fout:
-         merger.write(fout)
+with open(OUT_DIR+SAMPLENAME+"_Result.pdf", 'wb') as fout:
+    merger.write(fout)
